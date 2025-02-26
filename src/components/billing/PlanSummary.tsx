e45dab6d869cef3a4d436ab6920b9ea9712b26da
alexwox <alexander.woxstrom@gmail.com>
2025-02-26 21:32:10 +0100

    Plan summary, mock data

--- src/components/billing/PlanSummary.tsx
+++ src/components/billing/PlanSummary.tsx
@@ -1,8 +1,10 @@
 import { Tables } from "@datatypes.types";
 import { User } from "@supabase/supabase-js";
 import React from "react";
-import { Card, CardContent } from "../ui/card";
+import { Card, CardContent, CardFooter } from "../ui/card";
 import { Badge } from "../ui/badge";
+import { Progress } from "../ui/progress";
+import { Button } from "../ui/button";
 
 type Product = Tables<"products">;
 type Prices = Tables<"prices">;
@@ -29,13 +31,51 @@ interface PlanSummaryProps {
 function PlanSummary({ user, subscription, products }: PlanSummaryProps) {
   if (!subscription || subscription.status !== "active") {
     return (
-      <Card>
-        <CardContent>
-          <h3>
+      <Card className="max-w-5xl">
+        <CardContent className="px-5 py-4">
+          <h3 className="pb-4 text-base font-semibold flex flex-wrap items-center gap-x-2">
             <span>Plan Summary</span>
-            <Badge>No Plan</Badge>
+            <Badge variant={"secondary"} className="bg-primary/10">
+              No Plan
+            </Badge>
           </h3>
+          <div className="grid grid-cols-8 gap-4">
+            <div className="col-span-5 flex flex-col pr-12">
+              <div className="flex-1 text-sm font-normal flex w-full justify-between pb-1">
+                <span className="font-normal text-muted-foreground ml-1 lowercase">
+                  Image Generation credits left
+                </span>
+                <span className="font-medium">0 remaining</span>
+              </div>
+              <div className="mb-1 flex items-end">
+                <Progress value={0} className="w-full h-2" />
+              </div>
+            </div>
+
+            <div className="col-span-5 flex flex-col pr-12">
+              <div className="flex-1 text-sm font-normal flex w-full justify-between pb-1">
+                <span className="font-normal text-muted-foreground ml-1 lowercase">
+                  Model training credits left
+                </span>
+                <span className="font-medium">0 remaining</span>
+              </div>
+              <div className="mb-1 flex items-end">
+                <Progress value={0} className="w-full h-2" />
+              </div>
+            </div>
+
+            <div className="col-span-full flex flex-col">
+              Please upgrade to a plan to continue using the app.
+            </div>
+          </div>
         </CardContent>
+        <CardFooter className="border-t border-border px-4 py-3">
+          <span className="flex ml-auto flex-row">
+            <Button variant={"outline"} className="">
+              Upgrade
+            </Button>
+          </span>
+        </CardFooter>
       </Card>
     );
   }
