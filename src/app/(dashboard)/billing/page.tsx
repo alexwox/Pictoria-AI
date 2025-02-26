41ddeeded82b8329f4d27ecae199705505ee872a
alexwox <alexander.woxstrom@gmail.com>
2025-02-26 13:40:46 +0100

    preparing for stripe integration

--- src/app/(dashboard)/billing/page.tsx
+++ src/app/(dashboard)/billing/page.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 function BillingPage() {
-  return <div>page</div>;
+  return (
+    <section className="container mx-auto space-y-8">
+      <div className="">
+        <h1 className="text-3xl font-bold tracking-tight">Plans & Billing</h1>
+        <p className="text-muted-foreground">
+          Manage your subscription and billing information.
+        </p>
+      </div>
+    </section>
+  );
 }
 
 export default BillingPage;
